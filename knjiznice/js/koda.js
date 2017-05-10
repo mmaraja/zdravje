f3be9ed3fdb4701948860e30511166a66e6d9511
Dejan Lavbič <Dejan@lavbic.net>
2017-05-10 13:11:17 +0200

    Začetna verzija repozitorija

---
+++ knjiznice/js/koda.js
@@ -0,0 +1,37 @@
+
+var baseUrl = 'https://rest.ehrscape.com/rest/v1';
+var queryUrl = baseUrl + '/query';
+
+var username = "ois.seminar";
+var password = "ois4fri";
+
+
+/**
+ * Generiranje niza za avtorizacijo na podlagi uporabniškega imena in gesla,
+ * ki je šifriran v niz oblike Base64
+ *
+ * @return avtorizacijski niz za dostop do funkcionalnost
+ */
+function getAuthorization() {
+  return "Basic " + btoa(username + ":" + password);
+}
+
+
+/**
+ * Generator podatkov za novega pacienta, ki bo uporabljal aplikacijo. Pri
+ * generiranju podatkov je potrebno najprej kreirati novega pacienta z
+ * določenimi osebnimi podatki (ime, priimek in datum rojstva) ter za njega
+ * shraniti nekaj podatkov o vitalnih znakih.
+ * @param stPacienta zaporedna številka pacienta (1, 2 ali 3)
+ * @return ehrId generiranega pacienta
+ */
+function generirajPodatke(stPacienta) {
+  ehrId = "";
+
+  // TODO: Potrebno implementirati
+
+  return ehrId;
+}
+
+
+// TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
